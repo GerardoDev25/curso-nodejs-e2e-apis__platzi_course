1127647b46d67d58941871b5b30d2693d3ff255c
Gerardo Miranda <gerardodev25@gmail.com>
2023-03-31 19:17:52 -0400

    Buenas prácticas en pruebas E2E

--- e2e/app.e2e.js
+++ e2e/app.e2e.js
@@ -1,20 +1,30 @@
 const experss = require('express');
 const request = require('supertest');
 
-const app = experss();
-
-app.get('/hello', (req, res) => {
-  res.status(200).json({ name: 'nico' });
-});
-app.listen(9000);
-const api = request(app);
-
 describe('test for app', () => {
+  let app = null;
+  let server = null;
+  let api = null;
+
+  beforeEach(() => {
+    app = experss();
+
+    app.get('/hello', (req, res) => {
+      res.status(200).json({ name: 'nico' });
+    });
+    server = app.listen(9000);
+    api = request(app);
+  });
+
   test('GET /hello', async () => {
     const response = await api.get('/hello');
     expect(response).toBeTruthy();
     expect(response.statusCode).toEqual(200);
     expect(response.body.name).toEqual('nico');
     expect(response.headers['content-type']).toMatch(/json/);
+  });
+
+  afterEach(() => {
+    server.close();
   });
 });
