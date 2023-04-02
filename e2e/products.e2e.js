947c5d7fe5436ab5a6cf49de10b1d15ea89cb94e
Gerardo Miranda <gerardodev25@gmail.com>
2023-04-02 11:12:56 -0400

    Pruebas a la paginación

--- e2e/products.e2e.js
+++ e2e/products.e2e.js
@@ -25,6 +25,29 @@ describe('test for /products path', () => {
       expect(body.length).toEqual(products.length);
       expect(body[0].category).toBeTruthy();
     });
+
+    test('should return products with pagination "limit = 2 offset = 0"', async () => {
+      const limit = 2;
+      const offset = 0;
+      const { statusCode, body } = await api.get(
+        `/api/v1/products?limit=${limit}&offset=${offset}`
+      );
+
+      expect(statusCode).toEqual(200);
+      expect(body.length).toEqual(limit);
+      expect(body[0].category).toBeTruthy();
+    });
+    test('should return products with pagination "limit = 2 offset = 2"',  async () => {
+      const limit = 2;
+      const offset = 2;
+      const { statusCode, body } = await api.get(
+        `/api/v1/products?limit=${limit}&offset=${offset}`
+      );
+
+      expect(statusCode).toEqual(200);
+      expect(body.length).toEqual(limit);
+      expect(body[0].category).toBeTruthy();
+    });
   });
 
   afterAll(async () => {
